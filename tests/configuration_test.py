6f316daf272cf056d70250015de37027d1b6bfa7
Pedro Emanuel de Castro Faria Salgado <steenzout@ymail.com>
2014-06-11 19:21:00 -0400

    implemented tests for verzamelend.Config.getBool() and verzamelend.Config.getInt().

--- tests/configuration_test.py
+++ tests/configuration_test.py
@@ -1,5 +1,7 @@
 import tests
 
+import verzamelend
+
 
 from collections import namedtuple
 
@@ -18,4 +20,26 @@ class GetBoolTestCase(tests.BaseTestCase):
     def setUp(self):
         self.item1 = ConfigurationItem('key1', True)
         self.item2 = ConfigurationItem('key2', False)
+        self.item3 = ConfigurationItem('key3', 'true')
+        self.item4 = ConfigurationItem('key4', 'false')
+        self.config = MockConfig([self.item1, self.item2, self.item3, self.item4])
+
+    def test(self):
+        Config = verzamelend.Configuration(self.config)
+        self.assertTrue(Config.getBool('key1'))
+        self.assertFalse(Config.getBool('key2'))
+        self.assertTrue(Config.getBool('key3'))
+        self.assertFalse(Config.getBool('key4'))
+
+
+class GetIntTestCase(tests.BaseTestCase):
+
+    def setUp(self):
+        self.item1 = ConfigurationItem('key1', 1)
+        self.item2 = ConfigurationItem('key2', '2')
         self.config = MockConfig([self.item1, self.item2])
+
+    def test(self):
+        Config = verzamelend.Configuration(self.config)
+        self.assertEquals(Config.getInt('key1'), 1)
+        self.assertEquals(Config.getInt('key2'), 2)
